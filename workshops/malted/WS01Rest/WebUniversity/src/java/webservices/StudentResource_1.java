002249761add37ed66341a73b4cc78ed3101d195
Marcelo Malte <marcelo.malte11@gmail.com>
2022-05-20 22:12:28 -0500

    add WS01 Rest

---
+++ workshops/malted/WS01Rest/WebUniversity/src/java/webservices/StudentResource_1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/GenericResource.java to edit this template
+ */
+package webservices;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+
+/**
+ * REST Web Service
+ *
+ * @author marce
+ */
+public class StudentResource_1 {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of StudentResource_1
+     */
+    public StudentResource_1() {
+    }
+}
