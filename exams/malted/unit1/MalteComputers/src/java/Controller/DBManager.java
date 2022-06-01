dadfc619bd858c4ca3f40dd61e7b973d3c5b828d
Marcelo Malte <marcelo.malte11@gmail.com>
2022-06-01 14:17:50 -0500

    Examn U1

---
+++ exams/malted/unit1/MalteComputers/src/java/Controller/DBManager.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import ec.edu.espe.computersystem.model.Computer;
+
+/**
+ *
+ * @author marce
+ */
+public abstract class DBManager {
+    
+    public abstract boolean create(Computer computer);;
+    
+}
