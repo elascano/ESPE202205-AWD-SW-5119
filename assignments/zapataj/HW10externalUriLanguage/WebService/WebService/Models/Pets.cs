0d361d847bb47c9ac0be3b167f26c44a19e52601
Jonathan Ismael Zapata Sandoval <84453014+jizapata3@users.noreply.github.com>
2022-06-09 22:54:03 -0500

    HW10

---
+++ assignments/zapataj/HW10externalUriLanguage/WebService/WebService/Models/Pets.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebService
+{
+    public class Pets
+    {
+        public int ID { get; set; }
+        public string Name{ get; set; }
+        public string Type { get; set; }
+        public string Age { get; set; }
+        public bool Vaccinated { get; set; }
+
+        public Pets(int iD, string name, string type, string age, bool vaccinated)
+        {
+            ID = iD;
+            Name = name;
+            Type = type;
+            Age = age;
+            Vaccinated = vaccinated;
+        }
+
+        public Pets()
+        {
+        }
+    }
+}
