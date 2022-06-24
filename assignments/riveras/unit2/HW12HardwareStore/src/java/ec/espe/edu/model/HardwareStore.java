85e0dcd77d9208f7bc3c02ec1746a108c7f2cde8
STALIN BLADIMIR RIVERA VEGA <stalinrivera1980@gmail.com>
2022-06-24 15:54:51 -0500

    HW12

---
+++ assignments/riveras/unit2/HW12HardwareStore/src/java/ec/espe/edu/model/HardwareStore.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.espe.edu.model;
+
+/**
+ *
+ * @author User
+ */
+public class HardwareStore {
+    private int idItem;
+    private String name;
+    private String itemBrand;
+    private String description;
+    private double price;
+    private int inStock;
+
+    public HardwareStore(int idItem, String name, String itemBrand, String description, double price, int inStock) {
+        this.idItem = idItem;
+        this.name = name;
+        this.itemBrand = itemBrand;
+        this.description = description;
+        this.price = price;
+        this.inStock = inStock;
+    }
+
+    public HardwareStore() {
+        idItem = 0;
+        name = "";
+        itemBrand ="";
+        description ="";
+        price=0.0;
+        inStock = 0;
+    }
+
+    public int getIdItem() {
+        return idItem;
+    }
+
+    public void setIdItem(int idItem) {
+        this.idItem = idItem;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getItemBrand() {
+        return itemBrand;
+    }
+
+    public void setItemBrand(String itemBrand) {
+        this.itemBrand = itemBrand;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getInStock() {
+        return inStock;
+    }
+
+    public void setInStock(int inStock) {
+        this.inStock = inStock;
+    }
+    
+    
+    
+    
+    
+}
