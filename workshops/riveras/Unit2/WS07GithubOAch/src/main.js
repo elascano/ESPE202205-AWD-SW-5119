4cc76bb0479a287e4ca0476b2c3eb61cf22a3014
STALIN BLADIMIR RIVERA VEGA <stalinrivera1980@gmail.com>
2022-06-28 18:32:41 -0500

    WS07

---
+++ workshops/riveras/Unit2/WS07GithubOAch/src/main.js
@@ -0,0 +1,15 @@
+const URL_PARAMS = new URLSearchParams(windows.location.search);
+const TOKEN = URL_PARAMS.get('token');
+
+const show = (selector)=>{
+    document.querySelector(selector).style.display = 'block';
+};
+
+const hide = (selector)=>{
+    document.querySelector(selector).style.display = 'none';
+};
+
+if(TOKEN){
+    hide.apply('.content.unauthorized');
+    show('.content.authorized');
+}
