5d32c4cea30aaaefea85e045d690be668ff70126
AndresLopez13 <andresilopeza@gmail.com>
2022-07-01 14:12:05 -0500

    add WS08

---
+++ workshops/lopeza/Unit 2/WS08NodeRest/models/vehicle.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const vehiclesSchema = new mongoose.Schema({
+  plateId: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  model: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  
+})
+
+module.exports = mongoose.model('Vehicles', vehiclesSchema)
