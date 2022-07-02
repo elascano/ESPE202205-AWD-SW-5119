edb0f78764385832901ad14aed3d1dfc3997af89
AndresLopez13 <andresilopeza@gmail.com>
2022-07-01 21:32:41 -0500

    refactor WS09

--- workshops/lopeza/Unit 2/WS09NodeREST/server.js
+++ workshops/lopeza/Unit 2/WS09NodeREST/server.js
@@ -1,8 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const port = 3000;
-const app = express()
 const mongoose = require('mongoose')
+
+const app = express()
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
