228b57ffba32227faa9c532c865d756c92d1d462
imTheo <theomrosero@gmail.com>
2022-07-04 21:53:02 -0500

    ws08

---
+++ workshops/roserot/unit 2/WS07GithubOauth/index.js
@@ -0,0 +1,43 @@
+const axios = require('axios');
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+const PORT = 3000;
+
+
+app.use(express.static('src'));
+
+app.get('/',function (req,res) {
+    res.sendfile(path.join(__dirname,'./src/index.html'))
+});
+
+app.get('/auth',(req,res)=>{
+    res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`,
+    )
+});
+
+app.get('/oauth-callback',({query:{code}},res=>{
+    const body = {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_SECRET,
+        code,
+    };
+    const opts = {headers: {accept: 'application/json'}};
+    axios 
+        .post('https://github.com/login/oauth/access_token', body,opts)
+        .then((_res)=> _res.data.access_token)
+        .then((token)=>{
+            console.log('My token', token);
+            res.redirect(`/?token=${token}`);
+        })
+        .catch((err)=> res.status(500).json({err:err.message}));
+
+
+}))
+
+app.listen(PORT);
+console.log(`App listening on port ${PORT}`);
+
